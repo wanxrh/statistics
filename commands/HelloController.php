65991b97e0084827337aca5df6631143f83f730a
lvlinlin <549663504@qq.com>
2018-12-18 10:53:22 +0800

    edit

--- commands/HelloController.php
+++ commands/HelloController.php
@@ -83,5 +83,40 @@ class HelloController extends Controller
 
 
     }
+    public function actionGetkj(){
+        //$src = 'http://f.apiplus.net/bjpk10.json';
+        $src = 'https://kj.13322.com/trend/pk10.findIssueInfo.do';
+        //$src = 'http://e.apiplus.net/newly.do?token=t687d5a65d3b303c3k&code=bjpk10&format=json';
+        $src .= '?_='.time();
+        $json = file_get_contents(urldecode($src));
+        $data = json_decode($json,true);
 
+        if(!empty($data)){
+            //foreach($data as $k=>$item){
+                //先查询数据库有没有
+                $kj_one = Yii::$app->db->createCommand("SELECT * FROM xy_data2 WHERE `number`='{$data['predrawcode']}'")->queryOne();
+                //print_r($kj_one);exit;
+                if(empty($kj_one)){
+                    //当前数据库连接
+                    $db = Yii::$app->db;
+                    //生成命令查询器 这里其实可以直接根据 sql 进行构造
+                    $command = $db->createCommand();
+                    //单条插入
+                    $command->insert('xy_data2', [
+                        'type'=>'1',
+                        'time'=>$data['predrawtime'],
+                        'number'=>$data['preissue'],
+                        'data'=>$data['predrawcode'],
+                    ]);
+                    $result_insert = $command->execute();
+                    if($result_insert){
+                        echo "插入成功".PHP_EOL;
+                    }
+
+                }
+            //}
+        }
+
+
+    }
 }
