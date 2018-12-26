d2b0ec7764e2601782604160bbda24bc35c8e0ee
lvlinlin <549663504@qq.com>
2018-12-27 00:01:47 +0800

    edit

--- views/site/index.php
+++ views/site/index.php
@@ -72,7 +72,7 @@
                         <th colspan="10">开奖号码
                         </th>
                         <th>前三和值</th>
-                        <th>杀060723252627和值</th>
+                        <th>杀060709101920252627和值</th>
                         <!--<th>形态</th>-->
                         <th>两码合尾1234</th>
                         <th>0001111222路</th>
@@ -97,7 +97,7 @@
                                         <td width="5%"><?php echo $val?></td>
                                     <?php }?>
                                 <?php };?>
-                                <?php $hezhi=$kj_number[0]+$kj_number[1]+$kj_number[2];$hewei=['06','07','23','25','26','27']?>
+                                <?php $hezhi=$kj_number[0]+$kj_number[1]+$kj_number[2];$hewei=['06','07','09','10','19','20','25','26','27']?>
                                 <td><?php echo $hezhi?></td>
                             <?php if(\app\models\Service::shunza([$kj_number[0],$kj_number[1],$kj_number[2]])){$shunzi[] = 0;}else{$shunzi[] = 1;}?>
                             <?php if(!in_array($hezhi,$hewei)){
